ef7dd69a45376d4b7ccd14b2acb506fa50ff72c1
archana sudani <archanasudani@gmail.com>
2019-01-16 15:48:26 +0000

    My First Commit

---
+++ src/loveProgramming/Calculate.java
@@ -0,0 +1,34 @@
+package loveProgramming;
+
+public class Calculate {
+    private int a;
+    private int b;
+    private int c;
+
+    public Calculate() {
+            }
+    public Calculate(int a) {
+        this.a = a;
+        this.b = 0;
+        this.c = 0;
+    }
+    public Calculate(int a, int b) {
+        this.a = a;
+        this.b = b;
+        this.c = 0;
+    }
+    public Calculate(int a, int b, int c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+    public void getResult() {
+        if (this.a == 0 && this.b == 0 && this.c == 0) {
+            System.out.println("Calculations cant be done without any Inputs");
+        } else {
+            System.out.println("Result is " + (a + b + c));
+        }
+    }
+}
+
+
---
+++ src/loveProgramming/MyMain.java
@@ -0,0 +1,19 @@
+package loveProgramming;
+
+public class MyMain {
+    public static void main(String[] args)
+    {
+        Calculate c1 = new Calculate();
+        Calculate c2 = new Calculate(9);
+        Calculate c3 = new Calculate(12,03);
+        Calculate c4 = new Calculate(16,06,13);
+        c1.getResult();
+        c2.getResult();
+        c3.getResult();
+        c4.getResult();
+        
+
+    }
+
+    }
+
